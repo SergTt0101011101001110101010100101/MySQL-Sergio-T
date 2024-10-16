6cd4947e4b1eeee72d33db7983e41d6740a90ae8
SergioTt0101011101001110101010100101 <sergio.tatarchuk02@gmail.com>
2024-10-16 15:25:14 +0100

    Add files via upload

---
+++ sergio.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE Alemanha;
+
+Use Alemanha;
+Create Table Cidade(
+       Codigo_Postal char(25) primary key,
+       Numero_Residencia int(5) not null,
+       cidade char(30) not null
+);
+
+Select * from Alemanha;
